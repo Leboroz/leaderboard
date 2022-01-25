be9f25fdddc901ef282bff7bc6f49b15e7685c42
leonardo <albornozleo95@gmail.com>
2022-01-25 17:11:53 -0400

    Add header component

---
+++ src/modules/components/header.mjs
@@ -0,0 +1,10 @@
+export default function header() {
+  const header = document.createElement('section')
+  header.className = 'title padding-x'
+  header.innerHTML = `
+  <div class="header">
+    <h1 class="display-1">Leaderboard</h1>
+  </div>`
+
+  return header
+}
