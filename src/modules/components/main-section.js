59a4499ae60308a1f2be9232f1cfafe022a619e7
Leonardo <albornozleo95@gmail.com>
2022-01-28 12:34:11 -0400

    Add styles

--- src/modules/components/main-section.js
+++ src/modules/components/main-section.js
@@ -1,15 +1,16 @@
 const leaderBoardContainer = () => {
   const section = document.createElement('section');
-  section.className = 'leader-board-container padding-x';
+  section.className = 'leader-board-container';
 
   const leaderBoard = document.createElement('article');
   leaderBoard.className = 'container leader-board padding-x';
   leaderBoard.innerHTML = `
   <div class="recent-scores">
    <h2 class="display-2">Recent scores</h2>
-   <button id="refresh" type="button" class="btn">refresh</button>
+   <button id="refresh" type="button" class="btn">Refresh</button>
   </div>
   <ul id="board" class="board">
+    <div class="pop-up">There are no scores added</div>
   </ul>
   `;
 
@@ -17,9 +18,9 @@ const leaderBoardContainer = () => {
   form.className = 'container form-container padding-x';
   form.innerHTML = `
   <h2 class="display-2">Add your score</h2>
-  <form id="form" class="form">
+  <form id="form" class="form" autocomplete="off">
    <label ><input type="text" id="name" name="name" placeholder="Your name"/></label>
-   <label ><input type="text" id="score" name="score" placeholder="Your score"/></label>
+   <label ><input type="number" id="score" name="score" placeholder="Your score"/></label>
    <button type="submit" class="btn" />Submit</button>
   </form>
   `;
